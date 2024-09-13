8eda5fd3392ff67868059f891bd5a64d4b491ac7
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-09-13 20:24:10 -0300

    feat: add random-pivot versions for quicksort algorithm

--- data-structures-algorithms-ucsandiego/M4-4-quick-sort.py
+++ data-structures-algorithms-ucsandiego/M4-4-quick-sort.py
@@ -1,3 +1,4 @@
+import random
 def quick_sort(arr, lower, upper):
     if lower >= upper:
         return
@@ -16,4 +17,24 @@ def quick_sort(arr, lower, upper):
     quick_sort(arr, pivot_index+1, upper)
     return arr
 
+def quick_sort_random(arr, lower, upper):
+    if lower >= upper:
+        return
+
+    pivot_index = random.randint(lower, upper)
+    arr[lower], arr[pivot_index] = arr[pivot_index], arr[lower]
+    i = j = lower+1
+    while i <= upper:
+        if arr[i] <= arr[lower]:
+            arr[j], arr[i] = arr[i], arr[j]
+            j += 1
+        i += 1
+    arr[j-1], arr[lower] = arr[lower], arr[j-1]
+    pivot_index = j-1
+
+    quick_sort(arr, lower, pivot_index-1)
+    quick_sort(arr, pivot_index+1, upper)
+    return arr
+
 print(quick_sort([6, 4, 6, 9, 5, 3, 9, 1, 9, 7], 0, 9))
+print(quick_sort_random([6, 4, 6, 9, 5, 3, 9, 1, 9, 7], 0, 9))
--- data-structures-algorithms-ucsandiego/M4-5-quick-sort-3partition.py
+++ data-structures-algorithms-ucsandiego/M4-5-quick-sort-3partition.py
@@ -1,3 +1,4 @@
+import random
 # no_calls1 = 0
 # no_calls2 = 0
 def quick_sort3p_1st(arr, lower, upper):
@@ -29,7 +30,8 @@ def quick_sort3p_1st(arr, lower, upper):
 
 def quick_sort3p_2nd(arr, lower, upper):
     """
-    This second attemp version moves the pivot up as it finds new elements lesser than the pivot. The code is cleaner, but slightly more calls are necessary.
+    This second attemp version moves the pivot up as it finds new elements lesser than the pivot. The code is cleaner, but it seems like slightly more calls are necessary.
+    This code seems to be the more traditional implementation of quicksort 3 partitions.
     """
     # global no_calls2
     # no_calls2 += 1
@@ -50,8 +52,35 @@ def quick_sort3p_2nd(arr, lower, upper):
     quick_sort3p_2nd(arr, lower, m1-1)
     quick_sort3p_2nd(arr, m2+1, upper)
     return arr
+
+def quick_sort3p_random(arr, lower, upper):
+    """
+    This third version extends on the second version, adding an extra functionality of random pivots.
+    """
+    # global no_calls2
+    # no_calls2 += 1
+    if lower>=upper:
+        return
+
+    pivot_index = random.randint(lower, upper)
+    arr[lower], arr[pivot_index] = arr[pivot_index], arr[lower]
+    m1, i, m2 = lower, lower+1, upper
+    while i <= m2:
+        if arr[i] < arr[m1]:
+            arr[m1], arr[i] = arr[i], arr[m1]
+            m1 += 1
+            i += 1
+        elif arr[i] == arr[m1]:
+            i += 1
+        elif arr[i] > arr[m1]:
+            arr[i], arr[m2] = arr[m2], arr[i]
+            m2 -= 1
+    quick_sort3p_random(arr, lower, m1-1)
+    quick_sort3p_random(arr, m2+1, upper)
+    return arr
     
 
 print(quick_sort3p_1st([6, 4, 5, 6, 9, 5, 5, 5, 3, 9, 1, 9, 7], 0, 12))
 print(quick_sort3p_2nd([6, 4, 5, 6, 9, 5, 5, 5, 3, 9, 1, 9, 7], 0, 12))
+print(quick_sort3p_random([6, 4, 5, 6, 9, 5, 5, 5, 3, 9, 1, 9, 7], 0, 12))
 # print(no_calls1, no_calls2)
