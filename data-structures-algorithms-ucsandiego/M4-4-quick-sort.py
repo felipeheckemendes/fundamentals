d786d0dbb4981e42aae07741c351b115f81a64a6
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-09-13 05:40:56 -0300

    feat: add recursive quick sort algorithm (sorting in place)

---
+++ data-structures-algorithms-ucsandiego/M4-4-quick-sort.py
@@ -0,0 +1,19 @@
+def quick_sort(arr, lower, upper):
+    if lower >= upper:
+        return
+
+    pivot_index = 0
+    i = j = lower+1
+    while i <= upper:
+        if arr[i] <= arr[lower]:
+            arr[j], arr[i] = arr[i], arr[j]
+            j += 1
+        i += 1
+    arr[j-1], arr[lower] = arr[lower], arr[j-1]
+    pivot_index = j-1
+
+    quick_sort(arr, lower, pivot_index-1)
+    quick_sort(arr, pivot_index+1, upper)
+    return arr
+
+print(quick_sort([6, 4, 6, 9, 5, 3, 9, 1, 9, 7], 0, 9))
