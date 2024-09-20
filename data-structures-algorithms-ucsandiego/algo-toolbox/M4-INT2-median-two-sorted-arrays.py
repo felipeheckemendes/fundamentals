d52b2218131aee7c25eb015798b84e590d86908e
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-09-20 19:31:07 -0300

    fix(ucsandiego/4 divide-conquer): rewrite solution for assignement 4.3.2 Median of Two Sorted Arrays
    The previous implementation was incorrect.

--- data-structures-algorithms-ucsandiego/algo-toolbox/M4-INT2-median-two-sorted-arrays.py
+++ data-structures-algorithms-ucsandiego/algo-toolbox/M4-INT2-median-two-sorted-arrays.py
@@ -10,26 +10,30 @@ We won't merge the two arrays, because merging would result in linear time O(n).
 We are going to profit from the fact that the arrays are sorted, and use a binary search strategy to find the splits on the arrays (i and j), such that 
 -> all elements on array 1 that combe before and up to arr[i] and all elements before and up to arr[j] are smaller than all elements after the same index.
 """
-
 def median_of_sorted_arrays(arr1, arr2):
-    lower1, lower2 = 0, 0
-    upper1, upper2 = len(arr1) - 1, len(arr2) - 1
+    n = len(arr1) + len(arr2)
+    if len(arr2) < len(arr1): # Place the smallest arr as first, so that the median when len1 + len2 is odd will be r2, which is the longest
+        arr1, arr2 = arr2, arr1
     found = False
+    lower, upper = 0, len(arr1) - 1
     while not found:
-        mid1, mid2 = lower1 + (upper1-lower1)//2, lower2 + (upper2-lower2)//2
-        if max(arr1[mid1], arr2[mid2]) < min(arr1[mid1+1], arr2[mid2+1]):
+        mid1 = lower + (upper - lower+1)//2
+        l1 = float('-inf') if mid1 < 1 else arr1[mid1-1] # We use infinities so that the found True condition returns when we get to the edges
+        r1 = float('inf') if mid1 > len(arr1)-1 else arr1[mid1]
+
+        mid2 = n - len(arr1) - mid1 - 1
+        l2 = float('-inf') if mid2 < 1 else arr2[mid2-1]
+        r2 = float('inf') if mid2 > len(arr2)-1 else arr2[mid2]
+
+        if l1 <= r2 and l2 <= r1:
             found = True
-        else:
-            if arr1[mid1] > min(arr1[mid1+1], arr2[mid2+1]):
-                upper1 = mid1 - 1
-            if arr2[mid2] > min(arr1[mid1+1], arr2[mid2+1]):
-                upper2 = mid2 - 1
-            if arr1[mid1] < min(arr1[mid1+1], arr2[mid2+1]):
-                lower1 = mid1 + 1
-            if arr2[mid2] < min(arr1[mid1+1], arr2[mid2+1]):
-                lower2 = mid2 + 1
-    median = max(arr1[mid1], arr2[mid2])
+        elif l1 > r2:
+            upper = mid1 - 1
+        elif l2 > r1:
+            lower = mid1 + 1
+    if n % 2 == 0: median = (r1 + r2)/2
+    else: median = r2
     return median
 
-print(median_of_sorted_arrays([1, 2, 3, 4, 5], [4, 5, 6, 7, 8]))
+print(median_of_sorted_arrays([1, 10, 15, 30, 38], [5, 17, 20, 30, 45, 46]))
 
